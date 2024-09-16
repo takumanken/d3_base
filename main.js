222baeca7f10b2db1d914287c25766fa36ef0163
tak-w <iamtakuhisam@gmail.com>
2024-09-15 23:19:37 -0400

    chore: Update D3 base template with larger SVG dimensions

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const body = d3.select("body").style("padding", "30px");
 const title = body
   .append("h1")
   .style("font-size", "16px")
-  .text("Week 2 - Code Lab - Drawing Shapes");
+  .text("D3 Base Template");
 
 const svg = body
   .append("svg")
-  .attr("viewBox", "0 0 600 400")
-  .attr("width", 600)
-  .attr("height", 400)
+  .attr("viewBox", "0 0 1280 720")
+  .attr("width", 1280)
+  .attr("height", 720)
   .style("background", "white")
   .style("box-shadow", "0px 1px 2px #DDDDDD");
